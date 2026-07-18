48d6566c2e7c2cafe30dd625c258a56f1296641a
quantinium3 <quant@quantinium.dev>
2026-07-19 00:57:41 +0530

    add routes for parameters

--- src/router/routes.rs
+++ src/router/routes.rs
@@ -11,9 +11,21 @@ pub fn routes(state: AppState) -> Router {
         .route("/", get(handler::package::get_packages))
         .route("/{id}/install", post(handler::package::install_package))
         .route("/{id}/remove", post(handler::package::remove_package));
+    let parameter_router = Router::new()
+        .route(
+            "/",
+            get(handler::parameter::list_parameters).post(handler::parameter::create_parameter),
+        )
+        .route(
+            "/{*key}",
+            get(handler::parameter::get_parameter)
+                .put(handler::parameter::update_parameter)
+                .delete(handler::parameter::delete_parameter),
+        );
 
     let api_router = Router::new()
         .nest("/package", package_router)
+        .nest("/parameter", parameter_router)
         .nest("/sysinfo", sysinfo_router);
 
     Router::new()
