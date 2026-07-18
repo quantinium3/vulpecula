66799e41f5e49c0fc327f0e23f6a75e647e65458
quantinium3 <quant@quantinium.dev>
2026-07-18 21:32:35 +0530

    divided routes so that they can be nested

--- src/router/routes.rs
+++ src/router/routes.rs
@@ -1,11 +1,25 @@
-use axum::{Router, routing::get};
+use axum::{
+    Router,
+    routing::{get, post},
+};
 
 use crate::{handler, state::AppState};
 
 pub fn routes(state: AppState) -> Router {
+    let sysinfo_router = Router::new().route("/", get(handler::sysinfo::get_sysinfo));
+    let package_router = Router::new()
+        .route("/", get(handler::package::get_packages))
+        .route("/{id}/install", post(handler::package::install_package))
+        .route("/{id}/remove", post(handler::package::remove_package));
+
+    let api_router = Router::new()
+        .nest("/package", package_router)
+        .nest("/sysinfo", sysinfo_router);
+
     Router::new()
         .route("/", get(|| async { "Welcome to Vulpecula" }))
-        .route("/healthz", get(handler::health::health_handler))
-        .route("/version", get(handler::version::server_version_handler))
+        .route("/healthz", get(handler::health::get_health))
+        .route("/version", get(handler::version::get_server_version))
+        .nest("/api", api_router)
         .with_state(state)
 }
